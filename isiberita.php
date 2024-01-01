f69a64895c04f8af935e48348cf684bd0e027a47
yasminhafiza <154599009+yasminhafiza@users.noreply.github.com>
2024-01-01 19:50:05 +0700

    Update isiberita.php

--- isiberita.php
+++ isiberita.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Penelitian Gunadarma</title>
     <link rel="icon" href="assets/images/logo.png">
-    <link rel="stylesheet" href="css/style5.css">
+    <link rel="stylesheet" href="css/style6.css">
     <script src="javascript/script.js"></script>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
